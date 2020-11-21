5e0794b5c81c3387d3f1cbf0ab30c0a803979e3f
joseburg <joseaburgos01@outlook.com>
2020-11-21 13:45:59 +0100

    1

--- Ejemplo_1_y_2.R
+++ Ejemplo_1_y_2.R
@@ -3,25 +3,34 @@ library(readxl)
 library(stargazer)
 library(tidyverse)
 
-# Ejemplo 1 
+#<------------------------Ejemplo_1----------------------------->
+## Importando la base de datos 
 dt1<-read.csv("ejemplo_1.csv",sep = ";")
-View(dt1)
+View(dt1) # Ver la base de datos importada
 
+#Estimar el modelo 
+## Creando las variables
 salario<-dt1$salario
 educ<-dt1$educacion
 
 mod1<-lm(salario~educ)
+summary(mod1)
 stargazer(mod1,type = "text")
 
-# Ejemplo 2
-dt2<-read.csv("ejemplo_2.csv",sep = ";")
-View(dt2)
+# T critico:
+qt(0.05/2,933) # Valor absoluto 
 
+#<------------------------Ejemplo_2----------------------------->
+## Importando la base de datos 
+dt2<-read.csv("ejemplo_2.csv",sep = ";")
+View(dt2) # Ver la base de datos importada
+
+#Estimar el modelo 
+## Creando las variables
 peso<-dt2$peso_al_nacer
 cigs<-dt2$cigs_cons_sem
+
 mod2<-lm(peso~cigs)
-
-
 stargazer(mod2,type = "text")
 
 
